a9e22f00917bbfdbccba2db8876ed1be550178a5
Shawire <51907866+Shawire@users.noreply.github.com>
2021-02-16 21:43:35 +0800

    Create 4001.js

---
+++ univ/4001.js
@@ -0,0 +1,17 @@
+var univ_list = {
+    school: "吉林大学",
+    id: "4001",
+    link: [ {
+        name: "学校主页",
+        url: "https://www.tsinghua.edu.cn/"
+    }, {
+        name: "智慧学工",
+        url: "https://zhxg.jlu.edu.cn/xg/login"
+    }, {
+        name: "图书馆",
+        url: "http://lib.jlu.edu.cn/portal/index.aspx"
+    }, {
+        name: "吉大邮箱",
+        url: "https://mails.jlu.edu.cn/"
+    }]
+};
